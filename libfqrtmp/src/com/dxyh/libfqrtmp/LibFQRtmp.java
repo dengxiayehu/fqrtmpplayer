f2287347d55077e90b26548a8fbc5ef54eff1db9
dengxiayehu <dengxiayehu@yeah.net>
2016-03-30 14:57:24 +0800

    Add player project

--- libfqrtmp/src/com/dxyh/libfqrtmp/LibFQRtmp.java
+++ libfqrtmp/src/com/dxyh/libfqrtmp/LibFQRtmp.java
@@ -1,4 +1,20 @@
 package com.dxyh.libfqrtmp;
 
+import android.util.Log;
+
 public class LibFQRtmp {
+    private static final String TAG = LibFQRtmp.class.getSimpleName();
+    
+    static {
+        try {
+            System.loadLibrary("rtmp");
+            System.loadLibrary("fqrtmpjni");
+        } catch (UnsatisfiedLinkError ule) {
+            Log.e(TAG, "Can't load libraries: " + ule);
+            System.exit(1);
+        } catch (SecurityException se) {
+            Log.e(TAG, "Encountered as security issue when loading libraries: " + se);
+            System.exit(1);
+        }
+    }
 }
---
+++ player-android/src/com/dxyh/fqrtmpplayer/MainActivity.java
@@ -0,0 +1,29 @@
+package com.dxyh.fqrtmpplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_donate) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
